203052eb95b3fb57e2bf34cb33c53a47ee731ff6
wbscheid <wscheid@umw.edu>
2016-04-01 19:09:35 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,11 +3,15 @@ package TwitterAssignment;
  * @author Evan Shipman
  */
 public class Main {
+    // ReadInputFile method to be added after testing is completed tonight
     
-    
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         //GUI graphical = new GUI();
         //graphical.start();
+        ArrayList<User> userList = new ArrayList();
+        ArrayList<Message> messageList = new ArrayList();
+        userList = ReadInputFile("UsersFile.txt");
+        messageList = ReadInputFile("MessageFile.txt");
         
     }
     
