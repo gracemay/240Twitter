2d5831f33036fca508d2c314efb3153722046eeb
Jacqueline Coates <jacqueline.coates@gmail.com>
2016-04-06 11:23:53 -0400

    updated Yy is for copy

--- src/TwitterAssignment/Main.java
+++ src/TwitterAssignment/Main.java
@@ -7,45 +7,28 @@ import java.util.Date;
 import java.util.Scanner;
 /**
  * @author Evan Shipman
+ * Date: (4/3/16) Added pList and usernameList for LogUserIn, added methods to update the user and message files,
+ * some misc. formatting stuff also put some Main functionality and a method for searching messages.
  * @author William Scheid
+ * Date: (4/1/16) Added the updated input method and calls from the main method, with updated parameter read-ins.
  */
 public class Main {
-
-    private static ArrayList<String> pList = new ArrayList<>();
-    private static ArrayList<String> usernameList = new ArrayList<>();
+    //data members
+    private static ArrayList<String> pList = new ArrayList<String>();
+    private static ArrayList<String> usernameList = new ArrayList<String>();
     protected static SimpleDateFormat sdf = new SimpleDateFormat("mm/dd/yyyy");     //added to
     protected static SimpleDateFormat sdfMessages = new SimpleDateFormat("MM/dd/yyy hh:mm a");
     //added username and passwd to be data memebers.
     protected static String username = "", passwd = "";
 
-    /*
-        (4/3/16) Evan: Added the pList and usernameList for the LogUserIn, added methods to update the user and message files, some misc. formatting stuff
-                       also put some Main functionality and a method for searching messages. I wasn't able to finish the switch statements
-                       Currently there is one problem I'm noticing. When I run it, there is a NullPointerException that I think is somewhere around where
-                       it reads the input files. I can fix it but I won't be able to get to it again until tonight or early tomorrow morning
-                        The error could just be something I did and don't see yet.
 
-                       I sincerely apologize for any and all messy code pieces
-
-                        I have an idea to have display method in message class that takes user as input so it handles privacy and formatting
-    */
-
-    // (4/1/16) William: I added the updated input method and calls from the main method, with updated parameter read-ins.
-    //
-    // TO BE DONE: Next, the User and Message classes must be updated to incorporate the new CONSTRUCTOR parameters as follows:
-    // public Message(String user, int messageID, String message, int date, boolean privacy) {
-    // User(String usname, String pswd, String email, String regdate, String description, int numbFollwers, int numbFollwing, String followers, String following){
-    //
-    // This is probably a task for two different people(?), seeing as it then involves manipulating the class constructor
-    // and changing / adding methods.
-    //
     public static void main(String[] args) throws FileNotFoundException, NoSuchAlgorithmException, UnsupportedEncodingException, IOException {
         //GUI graphical = new GUI();
         //graphical.start();
-        ArrayList<User> userList = new ArrayList<>();
-        ArrayList<Message> messageList = new ArrayList<>();
-        userList = ReadInputFile("usersFile.txt");
-        messageList = ReadInputFile("messageFile.txt");
+        ArrayList<User> userList = new ArrayList<User>();
+        ArrayList<Message> messageList = new ArrayList<Message>();
+        userList = readUserInput("UsersFile.txt");
+        messageList = readMessageInput("MessageFile.txt");
 
         Scanner in = new Scanner(System.in);
 
@@ -86,7 +69,7 @@ public class Main {
     public static void WhileLoggedIn(ArrayList<Message> messageList, Scanner in){
         boolean done = false, success = false;
         Scanner command = new Scanner(System.in);
-        while(!success){
+        while (!success){
             System.out.print("What would you like to do?\n"
                     + "1.) Post Messages\n"
                     + "2.) View Messages\n"
@@ -98,32 +81,32 @@ public class Main {
                 case 1:
                     //added a try catch statement for java.io.IOException
                     boolean work = false;
-                    do{
-                        try{
+                    do {
+                        try {
                             //get public or private message
-                            System.out.print("Public message (Y/N)? ");
+                            System.out.print("Private message (Y/N)? ");
                             boolean privateMessage;
-                            String ans = in.next().toLowerCase();
-                            if(ans.equalsIgnoreCase("y"))
-                                privateMessage = true;
+                            in.nextLine();
+                            String ans = in.nextLine().toLowerCase();
+                            if(ans.equalsIgnoreCase("n"))
+                                privateMessage = false;
                             else
                                 privateMessage = true;
                             //ask for message
                             System.out.println("Please enter the message:");
-                            String content = in.next();
+                            String content = in.nextLine();
                             Message msg = new Message(username, (int) (System.nanoTime() % Integer.MAX_VALUE), content, System.currentTimeMillis(), privateMessage);
-                            messageList.add(msg);
                             //simple message ID for now
                             addMessage(messageList, msg); //until we come up with something
                             work = true;
                         }catch (java.io.IOException e){
                             System.out.println("Error With Message. Try again!");
                         }
-                    }while(!work);
+                    } while(!work);
                     break;                                                                                                        //better
                 case 2:
                     for (Message message : messageList) {
-                        if (message.privacy) {
+                        if (!message.privacy) {
                             System.out.println("@" + message.getUser() + "  on " + sdfMessages.format(new Date(message.getDate())));
                             System.out.println(message.getMessage() + "\n");
                         }
@@ -134,7 +117,7 @@ public class Main {
                     String[] terms = in.next().split(" ");
                     for (Message m : messageList)
                         if (hasTerms(m, terms))
-                            System.out.println(m.getUser() + "  on " + sdfMessages.format(new Date(m.getDate())) + m.getMessage() + "\n");
+                            System.out.println(m.getUser() + "  on " + sdfMessages.format(new Date(m.getDate())) + "\n" + m.getMessage() + "\n");
                     break;
                 case 4:
 
@@ -146,75 +129,70 @@ public class Main {
         }
     }
 
-
-
-    public static ArrayList ReadInputFile(String inputName) throws FileNotFoundException, NoSuchAlgorithmException, UnsupportedEncodingException {
-        ArrayList<User> uList = new ArrayList<>();
-        ArrayList<Message> mList = new ArrayList<>();
-
-        try {
-            if (inputName.equals("usersFile.txt")) {
-                File inFile = new File(inputName);
-                try (Scanner inTxt = new Scanner(inFile)) {
-                    //int firstLineIfNotApplicable = inTxt.nextInt();
-                    String username;
-                    String password;
-                    String email;
-                    String dateMade;
-                    String description;
-                    int followersCount;
-                    int followingCount;
-                    String followers;
-                    String following;
-                    while (inTxt.hasNext()) {
-                        username = inTxt.nextLine();
-                        password = inTxt.nextLine();
-                        email = inTxt.nextLine();
-                        dateMade = inTxt.nextLine();
-                        description = inTxt.nextLine();
-                        followersCount = Integer.parseInt(inTxt.nextLine());
-                        followingCount = Integer.parseInt(inTxt.nextLine());
-                        followers = inTxt.nextLine();
-                        following = inTxt.nextLine();
-                        User u = new User(username, password, email, description, followersCount, followingCount, followers, following);
-                        uList.add(u);
-                        pList.add((password));
-                        usernameList.add(username);
-                    }
-                }
-                return uList;
-            } else if (inputName.equals("messageFile.txt")) {
-                File inFile = new File(inputName);
-                try (Scanner inTxt = new Scanner(inFile)) {
-                    String user;
-                    int messageID;
-                    String message;
-                    long date;
-                    boolean privacy;
-                    while (inTxt.hasNext()) {
-                        user = inTxt.nextLine();
-                        messageID = Integer.parseInt(inTxt.nextLine());
-                        message = inTxt.nextLine();
-                        date = Long.parseLong(inTxt.nextLine());
-                        privacy = Boolean.parseBoolean(inTxt.nextLine());
-                        Message m = new Message(user, messageID, message, date, privacy);
-                        //Message m = new Message(user, messageID, message, privacy);
-                        mList.add(m);
-                    }
-                }
-                return mList;
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("No 'UsersFile.txt' or 'MessageFile.txt' found.");
+    public static ArrayList readMessageInput(String inputName) throws FileNotFoundException, NoSuchAlgorithmException, UnsupportedEncodingException {
+        ArrayList<Message> mList = new ArrayList<Message>();
+        File inFile2 = new File(inputName);
+        Scanner inTxt2 = new Scanner(inFile2);
+        String user;
+        int messageID;
+        String message;
+        long date;
+        boolean privacy;
+        while (inTxt2.hasNext()) {
+            user = inTxt2.nextLine();
+            System.out.println(user);
+            messageID = Integer.parseInt(inTxt2.nextLine());
+            //message = inTxt2.nextLine();
+            message = inTxt2.nextLine();
+            date = Long.parseLong(inTxt2.nextLine());
+            privacy = Boolean.parseBoolean(inTxt2.nextLine());
+            Message m = new Message(user, messageID, message, date, privacy);
+            mList.add(m);
         }
-
-        return null;
+        return mList;
     }
+
+    public static ArrayList readUserInput(String inputName) throws FileNotFoundException, NoSuchAlgorithmException, UnsupportedEncodingException {
+        ArrayList<User> uList = new ArrayList<User>();
+        File inFile = new File(inputName);
+        Scanner inTxt = new Scanner(inFile);
+        String username;
+        String password;
+        String email;
+        String dateMade;
+        String description;
+        int followersCount;
+        int followingCount;
+        String followers;
+        String following;
+        while (inTxt.hasNext()) {
+            username = inTxt.nextLine();
+            System.out.println(username);
+            password = inTxt.nextLine();
+            email = inTxt.nextLine();
+            dateMade = inTxt.nextLine();
+            //description = inTxt.nextLine();
+            description = inTxt.nextLine();
+            followersCount = Integer.parseInt(inTxt.nextLine());
+            followingCount = Integer.parseInt(inTxt.nextLine());
+            //followers = inTxt.nextLine();
+            followers = inTxt.nextLine();
+            //following = inTxt.nextLine();
+            following = inTxt.nextLine();
+
+            User u = new User(username, password, email, description, followersCount, followingCount, followers, following);
+            uList.add(u);
+            pList.add((password));
+            usernameList.add(username);
+        }
+        return uList;
+    }
+
 
     public static void addMessage(ArrayList<Message> mList, Message m) throws IOException
     {
         mList.add(m);
-        FileWriter fw = new FileWriter(new File("messageFile.txt"));
+        FileWriter fw = new FileWriter(new File("MessageFile.txt"));
         for (Message msg : mList)
         {
             fw.write(msg.getUser() + "\n");
@@ -261,10 +239,10 @@ public class Main {
     public static boolean hasTerms(Message msg, String[] terms)
     {
         for (int i = 0; i < terms.length; i++)
-            if (msg.getMessage().contains(terms[i]));
-        return true;
+            if (msg.getMessage().contains(terms[i]))
+                return true;
+        return false;
     }
-
 
 
 
