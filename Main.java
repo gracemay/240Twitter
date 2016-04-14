77d9a063d33c65824e83133a949a52023cc73cc0
Evan Shipman <eshipman@umw.edu>
2016-04-14 09:01:43 -0400

    GUI progress

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-//package TwitterAssignment;
+package TwitterAssignment;
 
 import java.io.*;
 import java.security.NoSuchAlgorithmException;
@@ -52,12 +52,12 @@ public class Main {
         messageList = new ArrayList<Message>();
         userList = readUserInput("UsersFile.txt");
         messageList = readMessageInput("MessageFile.txt");
-//        PreGUI.start();
-        Scanner in = new Scanner(System.in);
-
-        boolean done = startProgram(in);
-        if(done)
-            WhileLoggedIn(in);
+        PreGUI.start();
+//        Scanner in = new Scanner(System.in);
+//
+//        boolean done = startProgram(in);
+//        if(done)
+//            WhileLoggedIn(in);
     }
 
     //this method logs the user in
